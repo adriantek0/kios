9341191360263e4a920d341cc1f098bbf8ab63fd
Adrián <79924319+adriantek0@users.noreply.github.com>
2021-12-30 12:30:38 +0100

    tiktok integration

---
+++ src/Apps/TikTok/index.tsx
@@ -0,0 +1,14 @@
+export const TikTokApp = () => {
+    return (
+        <iframe
+            src='https://tiktok.com'
+            title='kiOS'
+            width='100%'
+            height='100%'
+            style={{
+                borderRadius: '0 0 6px 6px',
+                border: 'none',
+            }}
+        />
+    )
+}
